2690c88830a379b0125c1e916009159f3dd382b8
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-09-19 21:51:49 -0300

    fix: change atribute name of readByCountry of IModel

--- src/interfaces/IModel.ts
+++ src/interfaces/IModel.ts
@@ -1,7 +1,7 @@
 export interface IModel<T> {
   create(obj: T): Promise<T>,
   read(): Promise<T[] | null>,
-  readByCountry(_id: string): Promise<T[] | null>,
+  readByCountry(country: string): Promise<T[] | null>,
   readOneById(_id: string): Promise<T | null>,
   update(_id: string, obj: T): Promise<T | null>,
   delete(_id: string): Promise<T | null>,
