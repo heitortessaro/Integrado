cf96a0581e79d2459cc67e9b72f1de791ca40a3a
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-09-21 20:07:53 -0300

    fix: update fetchData to receave the country of interest

--- seeder/src/helpers/fetchData.js
+++ seeder/src/helpers/fetchData.js
@@ -1,15 +1,13 @@
 const axios = require('axios');
 
-const fetchData = async () => {
+const fetchData = async (country) => {
   let res;
   try {
-    res = await axios.get('http://universities.hipolabs.com/search?country=uruguay');
+    res = await axios.get(`http://universities.hipolabs.com/search?country=${country}`);
   } catch (err) {
     console.log('Error: ', err.message);
   }
-  return res;
+  return res.data;
 };
 
-module.exports = {
-  fetchData,
-};
+module.exports = fetchData;
