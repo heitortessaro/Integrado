0665a4d24042a0356f01ff47cf16a0e20455dc3e
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-09-20 23:05:31 -0300

    feat: use adpter to accept the external object keys by Controller methods

--- src/controllers/University.ts
+++ src/controllers/University.ts
@@ -1,60 +1,48 @@
 import { Request, Response } from 'express';
 import { IServiceUniversity } from '../interfaces/IServiceUniversity';
 import { IUniversity } from '../interfaces/IUniversity';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import {
+  convertToInternalPattern,
+  convertToExternalPattern,
+  convertToExternalArrayPattern,
+} from '../helper/objKeyConvertion';
 
 class UniversityController {
   constructor(private _service: IServiceUniversity<IUniversity>) { }
 
-  public async create(req: Request & { body: IUniversity }, res: Response<IUniversity>) {
-    // todo: fix body key 
-    const {
-      alphaTwoCode,
-      webPages,
-      name,
-      country,
-      domains,
-      stateProvince,
-    } = req.body;
-    const university = { alphaTwoCode, webPages, name, country, domains, stateProvince };
+  // public async create(req: Request, res: Response<IUniversity>) {
+  public async create(req: Request, res: Response) {
+    const university = convertToInternalPattern(req.body) as IUniversity;
     const result = await this._service.create(university);
-    return res.status(201).json(result);
+    return res.status(201).json(convertToExternalPattern(result));
   }
 
   public async read(req: Request, res: Response<IUniversity[]>) {
     if (req.query.country) {
       const result = await this._service.readByCountry(req.query.country as string);
-      return res.status(200).json(result);
+      return res.status(200).json(convertToExternalArrayPattern(result));
     }
     const result = await this._service.read();
-    return res.status(200).json(result);
+    return res.status(200).json(convertToExternalArrayPattern(result));
   }
 
-  public async readOneById(req: Request, res: Response<IUniversity>) {
+  public async readOneById(req: Request, res: Response) {
     const result = await this._service.readOneById(req.params.id);
-    return res.status(200).json(result);
+    return res.status(200).json(convertToExternalPattern(result));
   }
 
-  public async delete(req: Request, res: Response<IUniversity>) {
+  public async delete(req: Request, res: Response) {
     const result = await this._service.delete(req.params.id);
-    return res.status(204).json(result);
+    console.log(convertToExternalPattern(result));
+    return res.status(200).json(convertToExternalPattern(result));
   }
 
-  public async update(
-    req: Request & { body: IUniversity },
-    res: Response<IUniversity>,
-  ) {
-    // todo: fix body key 
-    const {
-      alphaTwoCode,
-      webPages,
-      name,
-      country,
-      domains,
-      stateProvince,
-    } = req.body;
-    const university = { alphaTwoCode, webPages, name, country, domains, stateProvince };
+  public async update(req: Request, res: Response) {
+    const university = convertToInternalPattern(req.body) as IUniversity;
     const result = await this._service.update(req.params.id, university);
-    return res.status(200).json(result);
+    return res.status(200).json(convertToExternalPattern(result));
   }
 }
 
